9cd195c0e2aa18981bbda482a2b3b22474e55fb6
Mohini-Aherrao <mohiniaherrao07@gmail.com>
2026-02-15 15:52:03 +0530

    Update main.tf

--- EKS-TF/main.tf
+++ EKS-TF/main.tf
@@ -20,7 +20,7 @@ data "aws_iam_policy_document" "assume_role" {
 
 # Create IAM Role for EKS Cluster
 resource "aws_iam_role" "example" {
-  name               = "my-eks-role"
+  name               = "eks-cluster-cloud"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
